08c5233dd618155b13da62d3ed4d65aff542b121
Starrain <2324532342@qq.com>
2024-07-17 23:19:12 +0800

    添加 myFunction 到 hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 // hello.c
 #include <stdio.h>
 
+void myFunction() {
+    printf("This is my new function.\n");
+}
+
 int main() {
+    myFunction();
     printf("Hello, World!\n");
     return 0;
 }
